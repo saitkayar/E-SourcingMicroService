8dc0ed08d8875dc82c3e2303dd4224e91fc5682c
sait kayar <85456550+saitkayar@users.noreply.github.com>
2023-02-27 09:30:43 +0300

    order

---
+++ Ordering.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Domain.Repositories.Base;
+using Ordering.Domain.Repositories;
+using Ordering.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ordering.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrderContext>(opt => opt.UseInMemoryDatabase(databaseName: "InMemoryDb"),
+                                                ServiceLifetime.Singleton,
+                                                ServiceLifetime.Singleton);
+
+            //services.AddDbContext<OrderContext>(options =>
+            //        options.UseSqlServer(
+            //            configuration.GetConnectionString("OrderConnection"),
+            //            b => b.MigrationsAssembly(typeof(OrderContext).Assembly.FullName)), ServiceLifetime.Singleton);
+
+            //Add Repositories
+            //services.AddTransient(typeof(IRepository<>), typeof(Repository<>));
+            //services.AddTransient<IOrderRepository, OrderRepository>();
+
+            return services;
+        }
+        }
+}
---
+++ Ordering.Infrastructure/Repositories/Base/Repository.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Entities.Base;
+using Ordering.Domain.Repositories.Base;
+using Ordering.Infrastructure.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories.Base
+{
+    public class Repository<T> : IRepository<T> where T : Entity
+    {
+        private readonly OrderContext context;
+
+        public Repository(OrderContext context)
+        {
+            this.context = context;
+        }
+        public async Task<T> AddAsync(T entity)
+        {
+            context.Set<T>().Add(entity);
+            await context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task DeleteAsync(T entity)
+        {
+            context.Set<T>().Remove(entity);
+            await context.SaveChangesAsync();
+         
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await context.Set<T>().ToListAsync();
+        }
+
+        public Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<T>> GetAsync(Expression<Func<T, bool>> predicate = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeString = null, bool disableTracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
